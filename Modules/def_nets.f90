cad14b6ef2aba4f0f082a3f40eb86d73e3944875
Alessandro Lunghi <lunghi.ale@gmail.com>
2021-08-11 15:23:50 +0100

    Default act func in def_nets changed to sigma

--- Modules/def_nets.f90
+++ Modules/def_nets.f90
@@ -7,7 +7,7 @@
           double precision               :: bias
           double precision               :: output
           double precision               :: grad_act
-          character(len=10)              :: activ='relu'
+          character(len=10)              :: activ='sigma'
           contains 
           procedure                      :: get_activity
          end type neuron
